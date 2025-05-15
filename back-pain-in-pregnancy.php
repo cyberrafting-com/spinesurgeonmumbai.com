3198450bb74315c7f428f088e086ddca18c3cc4d
deehub26 <deepika.dudhane@cyberrafting.com>
2025-05-15 12:12:19 +0530

    Updated bacl pain in pregnancy page

--- back-pain-in-pregnancy.php
+++ back-pain-in-pregnancy.php
@@ -5,12 +5,14 @@
 
 
 
-    <title>Back Pain treatment in Pregnancy in Mumbai by Dr. Vishal Kundnani</title>    
-<meta name="description" content="Dr. Vishal Kundnani provides top treatments for back pain during pregnancy. Safe and effective solutions in Mumbai.">  
-<meta name="keywords" content="Back Pain Treatment Mumbai, Dr. Vishal Kundnani, Spine Specialist Mumbai, Spine Surgeon Mumbai, Spine Clinic Mumbai, Micro Endoscopic Spine Surgery Doctor Mumbai, Spine Disease Doctor Mumbai, Spine Surgery Specialist Mumbai, Top Spine Doctor Bandra, Spine Specialist Bandra, Spine Treatment Mumbai, Spine Disease Treatment Mumbai">
-<link rel="canonical" href="https://www.spinesurgeonmumbai.com/back-pain-in-pregnancy.php" />
+  <title>Back Pain treatment in Pregnancy in Mumbai by Dr. Vishal Kundnani</title>
+  <meta name="description"
+    content="Dr. Vishal Kundnani provides top treatments for back pain during pregnancy. Safe and effective solutions in Mumbai.">
+  <meta name="keywords"
+    content="Back Pain Treatment Mumbai, Dr. Vishal Kundnani, Spine Specialist Mumbai, Spine Surgeon Mumbai, Spine Clinic Mumbai, Micro Endoscopic Spine Surgery Doctor Mumbai, Spine Disease Doctor Mumbai, Spine Surgery Specialist Mumbai, Top Spine Doctor Bandra, Spine Specialist Bandra, Spine Treatment Mumbai, Spine Disease Treatment Mumbai">
+  <link rel="canonical" href="https://www.spinesurgeonmumbai.com/back-pain-in-pregnancy.php" />
 
-<script type="application/ld+json">
+  <script type="application/ld+json">
 {
   "@context": "https://schema.org",
   "@type": "MedicalWebPage",
@@ -50,7 +52,7 @@
 }
 </script>
 
-<script type="application/ld+json">
+  <script type="application/ld+json">
 {
   "@context": "https://schema.org",
   "@type": "FAQPage",
@@ -83,69 +85,119 @@
 }
 </script>
 
-<?php include 'includes/head.php'; ?>
-   
+  <?php include 'includes/head.php'; ?>
+
 </head>
 
 
 <body>
 
-    <!-- Google Tag Manager (noscript) -->
-    <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6PXG4VM"
-            height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
-    <!-- End Google Tag Manager (noscript) -->
-    <header class="header col-lg-12 col-md-12 col-sm-12 col-xs-12 affix1" data-spy="affix1" data-offset-top="75">
+  <!-- Google Tag Manager (noscript) -->
+  <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N6PXG4VM" height="0" width="0"
+      style="display:none;visibility:hidden"></iframe></noscript>
+  <!-- End Google Tag Manager (noscript) -->
+  <header class="header col-lg-12 col-md-12 col-sm-12 col-xs-12 affix1" data-spy="affix1" data-offset-top="75">
 
-        <?php include 'includes/header.php'; ?>
+    <?php include 'includes/header.php'; ?>
 
-    </header>
+  </header>
 
-    <div class="banner-wrap1">
-		<div class="container">
-			<div class="row">
-				<h2>We <span>Treat</span></h2> 
-				<ul class="breadcrumb1">
-					<li><a href="index.php">Home</a></li>
-					<li><a href="back-pain-we-treat.php">Back Pain</a></li>
-				</ul>
-			</div>
-		</div>
-	</div>
+  <div class="banner-wrap1">
+    <div class="container">
+      <div class="row">
+        <h2>We <span>Treat</span></h2>
+        <ul class="breadcrumb1">
+          <li><a href="index.php">Home</a></li>
+          <li><a href="back-pain-we-treat.php">Back Pain</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
 
 
-    <section class="drtoafndrb col-lg-12 col-md-12 col-sm-12 col-xs-12">
-        <div class="container">
+  <section class="drtoafndrb col-lg-12 col-md-12 col-sm-12 col-xs-12">
+    <div class="container">
 
-            <div class="row">
-                <div class="cliclskil1 mt-3">
-                <div class="col-lg-8 col-md-8 col-sm-8">
-			<h1>Back Pain in Pregnancy</h1>
-			<p class="mt-1">The good news is, your baby is growing. That's exactly what should be happening, but it can still be tough on your back.You've got lots of company, most pregnant women experience back pain, usually starting in the second half of pregnancy.</p>
-			<p>You should know that there are things you can do to minimize your back pain.</p>
-			<h1>Causes of Back Pain in Pregnant Women:</h1>
-			<p class="mt-1">Pregnancy back pain typically happens where the pelvis meets your spine, at the sacroiliac joint.</p>
-			<p>There are many possible reasons why it happens. Here are some of the more likely causes:</p>
-			<ul>
-				<li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite" alt="pic" style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Weight gain : During a healthy pregnancy, women typically gain between 25 and 35 pounds. The spine has to support that weight. That can cause lower back pain. The weight of the growing  baby  and uterus also puts pressure on the blood vessels and nerves in the pelvis and back.</li>
-				<li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite" alt="pic" style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Posture changes : Pregnancy shifts your center of gravity. As a result, you may gradually — even without noticing — begin to adjust your posture and the way you move. This may result in back pain or strain.</li>
-				<li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite" alt="pic" style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Hormone changes : During pregnancy, your body makes a hormone called relaxin that allows ligaments in the pelvic area to relax and the joints to become looser in preparation for the birth process. The same hormone can cause ligaments that support the spine to loosen, leading to instability and pain.</li>
-				<li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite" alt="pic" style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Muscle separation : As the uterus expands, two parallel sheets of muscles (the rectal abdominis muscles), which run from the rib cage to the pubic bone, may separate along the center seam. This separation may worsen back pain.</li>
-				<li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite" alt="pic" style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Stress : Emotional stress can cause muscle tension in the back, which may be felt as back pain or back spasms. You may find that you experience an increase in back pain during stressful periods of your pregnancy.</li>
-					</ul>
-			
-			</div>
+      <div class="row">
+        <div class="cliclskil1 mt-3">
+          <div class="col-lg-8 col-md-8 col-sm-8">
+            <h2>Back Pain Treatment during Pregnancy</h2>
+            <div class="container ba_image mt-4">
 
-                    <div class="col-lg-4 col-md-4 col-sm-4">
+              <img src="images/back-pain-in-pregnancy.jpg"
+                alt="Back Pain Treatment during pregnancy in Mumbai by Dr. Vishal Kundnani"
+                class=" img-fluid rounded shadow-sm" style="max-width: 100%; height: auto;">
 
-                        <?php include 'includes/we-treat.php'; ?>
-                    </div>
-                </div>
             </div>
+            <h1>Back Pain in Pregnancy</h1>
+            <p class="mt-1">The good news is, your baby is growing. That's exactly what should be happening, but it can
+              still be tough on your back.You've got lots of company, most pregnant women experience <a href="https://spinesurgeonmumbai.com/spine-tip-of-the-day-back-pain.php" target="_blank">back pain</a>, usually
+              starting in the second half of pregnancy.</p>
+            <p>You should know that there are things you can do to minimize your <a href="https://spinesurgeonmumbai.com/back-pain-we-treat.php" target="_blank">back pain</a>.</p>
+            <div class="container ba_image mt-4">
+
+              <img src="images/Lower-Back-Pain-During-Pregnancy.jpg"
+                alt="Back Pain Treatment during pregnancy in Mumbai by Dr. Vishal Kundnani"
+                class=" img-fluid rounded shadow-sm" style="max-width: 100%; height: auto;">
+
+            </div>
+            <h2>Causes of Back Pain in Pregnant Women :</h2>
+            <p class="mt-1">Pregnancy back pain typically happens where the pelvis meets your spine, at the sacroiliac
+              joint.</p>
+            <p>There are many possible reasons why it happens. Here are some of the more likely causes:</p>
+            <div class="container ba_image mt-4">
+
+              <img src="images/causes-of-back-pain-in-pregnant-women.jpg"
+                alt="Causes of Back Pain during pregnancy treated by Dr. Vishal Kundnani in Mumbai"
+                class=" img-fluid rounded shadow-sm" style="max-width: 100%; height: auto;">
+
+            </div>
+            <ul>
+              <li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite"
+                  alt="pic"
+                  style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Weight gain
+                : During a healthy pregnancy, women typically gain between 25 and 35 pounds. The spine has to support
+                that weight. That can cause lower back pain. The weight of the growing baby and uterus also puts
+                pressure on the blood vessels and nerves in the pelvis and back.</li>
+              <li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite"
+                  alt="pic"
+                  style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Posture
+                changes : Pregnancy shifts your center of gravity. As a result, you may gradually — even without
+                noticing — begin to adjust your posture and the way you move. This may result in back pain or strain.
+              </li>
+              <li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite"
+                  alt="pic"
+                  style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Hormone
+                changes : During pregnancy, your body makes a hormone called relaxin that allows ligaments in the pelvic
+                area to relax and the joints to become looser in preparation for the birth process. The same hormone can
+                cause ligaments that support the spine to loosen, leading to instability and pain.</li>
+              <li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite"
+                  alt="pic"
+                  style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Muscle
+                separation : As the uterus expands, two parallel sheets of muscles (the rectal abdominis muscles), which
+                run from the rib cage to the pubic bone, may separate along the center seam. This separation may worsen
+                back pain.</li>
+              <li><img src="img/founder_li_icon.png" class="wow flipInX animated animated" data-wow-iteration="infinite"
+                  alt="pic"
+                  style="visibility: visible; animation-iteration-count: infinite; animation-name: flipInX;">Stress :
+                Emotional stress can cause muscle tension in the back, which may be felt as back pain or back spasms.
+                You may find that you experience an increase in back pain during stressful periods of your pregnancy.
+              </li>
+            </ul>
+<br><p>For those experiencing <strong>back pain during pregnancy</strong>, seeking expert care can make a significant difference. Under the guidance of <a href="https://spinesurgeonmumbai.com/about-doctor.php" target="_blank">Dr. Vishal Kundnani</a>, a renowned spine specialist in Mumbai, our specialized pregnancy <a href="https://spinesurgeonmumbai.com/index.php" target="_blank">spine care center in Mumbai</a>, offers compassionate, patient-focused treatment tailored to the unique needs of expectant mothers. With advanced technology and a team of experienced professionals, we ensure safe and effective relief from pregnancy-related back pain—supporting both your comfort and well-being throughout every stage.</p>
+          </div>
+
+          <div class="col-lg-4 col-md-4 col-sm-4">
+
+            <?php include 'includes/we-treat.php'; ?>
+          </div>
         </div>
-    </section>
+      </div>
+    </div>
+  </section>
 
 
-    <?php include 'includes/footer.php'; ?>
+  <?php include 'includes/footer.php'; ?>
 </body>
 
 
