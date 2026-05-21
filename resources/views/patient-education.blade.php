27a3b1b74fb197a917ad2d837311208ce59a81ed
Suchita Chandrakant Hamidawade <suchita@cyberrafting.com>
2026-05-21 16:31:17 +0530

    add patient education video gallery page with structured data support

--- resources/views/patient-education.blade.php
+++ resources/views/patient-education.blade.php
@@ -75,6 +75,16 @@ Mumbai')
             <div class="col-lg-4 col-md-4 col-sm-6">
                 <div class="gallery1 shorts-card">
                     <iframe width="100%" height="550"
+                        src="https://www.youtube.com/embed/rKNND9d5XCk?si=yH1ngFXJRof_nX3a" title="YouTube video player"
+                        frameborder="0"
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        allowfullscreen></iframe>
+                    <h1>Best Desk Job Exercises for Neck Pain &amp; Posture Correction (In Hindi) | Dr. Aditi Tanna</h1>
+                </div>
+            </div>
+            <div class="col-lg-4 col-md-4 col-sm-6">
+                <div class="gallery1 shorts-card">
+                    <iframe width="100%" height="550"
                         src="https://www.youtube.com/embed/wHBX8szfVBQ?si=c1bEQgCy90wOioqU" title="YouTube video player"
                         frameborder="0"
                         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
